f94089d21897d37b1fe076ec200af3800588f5bf
natalie_jay <njr@uchicago.edu>
2023-03-26 16:33:18 -0500

    first code created
    This first code is nothing important but it is only useful for me to learn this.

---
+++ natalies_test.py
@@ -0,0 +1,17 @@
+# This is a python code for testing an inital program
+'''
+For multiple line comments, use three apostrophes
+
+Author : Natalie J. Reyes
+Date : 3/26/2023
+Version : 0.0.1
+
+'''
+
+my_first_variable = 16
+my_second_variable = 423
+
+their_sum = my_first_variable + my_second_variable
+print('What\'s up!!')
+print('Welcome to my first program')
+print(f'sum of {my_first_variable} and {my_second_variable} is {their_sum}')
